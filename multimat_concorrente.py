d2bb6c5c1590f51be958a2acbbd0ba9aa7962c67
Marciel Manoel Leal <badboytux@A3032014044611L.imd.local>
2017-10-04 18:00:09 -0300

    end class

--- multimat_concorrente.py
+++ multimat_concorrente.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
 import sys
-import squarematrix
 import multiplier
+import squarematrix
+
+PATH="Matrizes/"
 
 try:
-	PATH="Matrizes/"
-
-	#try:
 	size=int(sys.argv[1])
 	threadNum=int(sys.argv[2])
-	if (size<4 or size>2048 or threadNum<=1):
+	if (size<4 or size>2048 or threadNum<=1 or (size & (size-1))!=0):
 		print("It is required an argument of type 2^n, where n is in [2,11] and a number of threads greater than 1:")
 		print("./"+sys.argv[0]+" <integer> <number_of_threads>")
 		sys.exit(1)
--- multimat_sequencial.py
+++ multimat_sequencial.py
@@ -3,13 +3,17 @@
 import sys
 import multiplier
 import squarematrix
+import time
 
+tstart=time.timer()
 PATH="Matrizes/"
 
 try:
 	size=int(sys.argv[1])
-	if (size<4 or size>2048):
-		raise IndexError()
+	if (size<4 or size>2048 or (size & (size-1))!=0):
+		print("It is required an argument of type 2^n, where n is in [2,11]")
+		print("./"+sys.argv[0]+" <integer>")
+		sys.exit(1)
 	fileName=sys.argv[1]+"x"+sys.argv[1]+".txt"
 	arqA=open(PATH+"A"+fileName,"r")
 	arqB=open(PATH+"B"+fileName,"r")
@@ -22,10 +26,8 @@ try:
 	print(C)
 
 	sys.exit(0)
-except IndexError:
-	print("It is required an argument of type 2^n, where n is in [2,11]. Example:")
-	print("./"+sys.argv[0]+" <integer>")
-	sys.exit(1)
 except FileNotFoundError:
 	print("File not found")
 	sys.exit(2)
+tend=time.timer()
+
